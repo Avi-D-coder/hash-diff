5da9e0a9d9867eec82588c071055cb281acd146d
Avi ד <avi.the.coder@gmail.com>
2019-01-08 06:07:55 -0500

    Backwards case

--- src/lib.rs
+++ src/lib.rs
@@ -2,7 +2,7 @@ use std::cmp::Ordering;
 use std::iter::once;
 
 use either::Either;
-use fasthash::murmur3::Murmur3_x86_32;
+use fasthash::murmur3::Murmur3Hasher_x86_32;
 use itertools::{EitherOrBoth, EitherOrBoth::*, Itertools};
 use perfect_hash::PerfectHasher32;
 use wu_diff::*;
@@ -95,14 +95,16 @@ impl LineDiff for &str {
         // aba
         // ababa
 
+        // unwrap is safe due to early return if is_left or is_right
+        let fw_fst_nq = fw_item.as_ref().both().unwrap();
+        // unwrap is safe because if forward eq is longer then backward and is Both
+        // then bw both() is Some
+        let bw_fst_nq = bw_item.clone().both().unwrap();
+
+        let mut shorter_len = None;
+
         if fw_index >= bw_index {
             let fw = once((fw_index, fw_item.clone())).chain(fw);
-
-            // unwrap is safe due to early return if is_left or is_right
-            let fw_fst_nq = fw_item.as_ref().both().unwrap();
-            // unwrap is safe because if forward eq is longer then backward and is Both
-            // then bw both() is Some
-            let bw_fst_nq = bw_item.both().unwrap();
 
             // if eq segments are overlapping
             if fw_fst_nq.0.as_ptr() >= bw_fst_nq.0.as_ptr()
@@ -115,8 +117,7 @@ impl LineDiff for &str {
             // old: "abc"
             // new: "ab-bc"
 
-            let mut shorter_len = None;
-            fw.take_while(|(i, e)| {
+            let changed = fw.take_while(|(i, e)| {
                 let mut not_greater = |a: &str, b: *const u8| match a.as_ptr().cmp(&b) {
                     Ordering::Less => true,
                     Ordering::Greater => false,
@@ -140,12 +141,70 @@ impl LineDiff for &str {
                     // Stop when backwards equal set <= then remainder of smaller side (old/new).
                     shorter_len.map_or(true, |len| fw_index <= (len - i) + 1)
                 }
-            })
-            .collect::<Vec<_>>(); // TODO into perfect_hash and vec<hash>
+            });
+            let mut ph = PerfectHasher32::new(Murmur3Hasher_x86_32::default());
 
-            unimplemented!()
+            let mut changed_old = Vec::with_capacity(100);
+            let mut changed_new = Vec::with_capacity(100);
+            for (_, e) in changed {
+                e.map_left(|l| changed_old.push(ph.unique_id(l)))
+                    .map_right(|r| changed_new.push(ph.unique_id(r)));
+            }
+
+            // Return diffed result
+            wu_diff::diff(&changed_old, &changed_new)
         } else {
-            unimplemented!()
+            let bw = once((bw_index, bw_item.clone())).chain(bw);
+
+            // if eq segments are overlapping
+            if bw_fst_nq.0.as_ptr() >= fw_fst_nq.0.as_ptr()
+                || bw_fst_nq.1.as_ptr() >= fw_fst_nq.1.as_ptr()
+            {
+                // fw.map(|(_, z)| z);
+                unimplemented!()
+            }
+
+            // old: "abc"
+            // new: "ab-bc"
+
+            let changed = bw.take_while(|(i, e)| {
+                let mut not_greater = |a: &str, b: *const u8| match a.as_ptr().cmp(&b) {
+                    Ordering::Less => true,
+                    Ordering::Greater => false,
+                    Ordering::Equal => {
+                        if shorter_len.is_none() {
+                            shorter_len = Some(i + fw_index + 2)
+                        };
+                        false
+                    }
+                };
+
+                let not_past_back_matched = e
+                    .as_ref()
+                    .map_left(|a| not_greater(a, fw_fst_nq.0.as_ptr()))
+                    .map_right(|b| not_greater(b, fw_fst_nq.1.as_ptr()))
+                    .reduce(|a, b| a && b);
+
+                if not_past_back_matched {
+                    true
+                } else {
+                    // Stop when backwards equal set <= then remainder of smaller side (old/new).
+                    shorter_len.map_or(true, |len| bw_index <= (len - i) + 1)
+                }
+            });
+            let mut ph = PerfectHasher32::new(Murmur3Hasher_x86_32::default());
+
+            let mut changed_old = Vec::with_capacity(100);
+            let mut changed_new = Vec::with_capacity(100);
+            for (_, e) in changed {
+                e.map_left(|l| changed_old.push(ph.unique_id(l)))
+                    .map_right(|r| changed_new.push(ph.unique_id(r)));
+            }
+
+            changed_new.reverse();
+            changed_old.reverse();
+            // Return diffed result
+            wu_diff::diff(&changed_old, &changed_new)
         }
     }
 }
